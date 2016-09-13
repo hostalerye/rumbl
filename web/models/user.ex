ff76bb7faef105c21bad8a4632ab78bd4fbfa89b
Eric Hostalery <hostalerye@gmail.com>
2016-09-13 19:21:29 +0200

    Minor fix + add has_many relationship to user

--- web/models/user.ex
+++ web/models/user.ex
@@ -6,20 +6,21 @@ defmodule Rumbl.User do
 		field :username, :string
 		field :password, :string, virtual: true
 		field :password_hash, :string
+		has_many :videos, Rumbl.Video
 
 		timestamps
 	end
 
-	def changeset(model, params) do
+	def changeset(model, params \\ %{}) do
 		model
-		|> cast(params, ~w(name username), [])
+		|> cast(params, [:name, :username], [])
 		|> validate_length(:username, min: 1, max: 20)
 	end
 
-	def registration_changeset(model, params) do
+	def registration_changeset(model, params \\ %{}) do
 		model
 		|> changeset(params)
-		|> cast(params, ~w(password), [])
+		|> cast(params, [:password], [])
 		|> validate_length(:password, min: 6, max: 100)
 		|> put_pass_hash()
 	end
